dafd5d32d11d4b9b846193a1acc4e079ddb9bb66
dan_kucherenko <daniil.ukraine@gmail.com>
2023-03-02 22:03:33 +0200

    User model added

---
+++ Lab01Kucherenko/Lab01Kucherenko/Models/User.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using KMA.Lab01Kucherenko.Tools;
+
+namespace KMA.Lab01Kucherenko.Models
+{
+    internal class User
+    {
+        #region Fields
+
+        private int _age;
+        private DateTime _dob = DateTime.Today;
+        private ZodiacSigns _zodiac;
+        private ChineseZodiacSigns _chineseZodiac;
+
+        #endregion
+
+        #region Properties
+
+        public int Age
+        {
+            get => _age;
+            set => _age = value;
+        }
+
+        public DateTime DateOfBirth
+        {
+            get => _dob;
+            set => _dob = value;
+        }
+
+        public ZodiacSigns Zodiac
+        {
+            get => _zodiac;
+            set => _zodiac = value;
+        }
+
+        public ChineseZodiacSigns ChineseZodiac
+        {
+            get => _chineseZodiac;
+            set => _chineseZodiac = value;
+        }
+
+        #endregion
+    }
+}
