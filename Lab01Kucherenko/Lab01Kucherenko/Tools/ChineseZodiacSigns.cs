b8a86edd309c42c8b63bfb478497f1c9e2d3eb55
dan_kucherenko <daniil.ukraine@gmail.com>
2023-03-02 15:31:38 +0200

    Add signs enum

---
+++ Lab01Kucherenko/Lab01Kucherenko/Tools/ChineseZodiacSigns.cs
@@ -0,0 +1,18 @@
+﻿namespace KMA.Lab01Kucherenko.Tools
+{
+    public enum ChineseZodiacSigns
+    {
+        Rat,
+        Ox,
+        Tiger,
+        Rabbit,
+        Dragon,
+        Snake,
+        Horse,
+        Goat,
+        Monkey,
+        Rooster,
+        Dog,
+        Pig
+    }
+}
---
+++ Lab01Kucherenko/Lab01Kucherenko/Tools/ZodiacSigns.cs
@@ -0,0 +1,18 @@
+﻿namespace KMA.Lab01Kucherenko.Tools
+{
+    public enum ZodiacSigns
+    {
+        Aries,
+        Taurus,
+        Gemini,
+        Cancer,
+        Leo,
+        Virgo,
+        Libra,
+        Scorpio,
+        Sagittarius,
+        Capricorn,
+        Aquarius,
+        Pisces
+    }
+}
