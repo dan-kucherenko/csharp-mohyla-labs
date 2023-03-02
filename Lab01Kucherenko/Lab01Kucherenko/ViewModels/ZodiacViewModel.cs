47a09877f36d0d7eb2a7923449e6a63be3f42a85
dan_kucherenko <daniil.ukraine@gmail.com>
2023-03-02 22:51:13 +0200

    ViewModel with Model's props added

---
+++ Lab01Kucherenko/Lab01Kucherenko/ViewModels/ZodiacViewModel.cs
@@ -0,0 +1,49 @@
+﻿using KMA.Lab01Kucherenko.Models;
+using KMA.Lab01Kucherenko.Tools;
+using System;
+
+namespace KMA.Lab01Kucherenko.ViewModels
+{
+    internal class ZodiacViewModel
+    {
+        private User _user = new User();
+
+        #region Properties
+
+        public int Age
+        {
+            get => _user.Age;
+            set => _user.Age = value;
+        }
+
+        public DateTime DateOfBirth
+        {
+            get => _user.DateOfBirth;
+            set => _user.DateOfBirth = value;
+        }
+
+        public ZodiacSigns Zodiac
+        {
+            get => _user.Zodiac;
+            set => _user.Zodiac = value;
+        }
+
+        public ChineseZodiacSigns ChineseZodiac
+        {
+            get => _user.ChineseZodiac;
+            set => _user.ChineseZodiac = value;
+        }
+
+        #endregion
+
+        public void DateChanged()
+        {
+            CalculateAge(DateOfBirth);
+        }
+
+        private void CalculateAge(DateTime selectedDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
