25293dbaeed67ed1f707567ec928859b6f4f9467
dan_kucherenko <daniil.ukraine@gmail.com>
2023-03-02 22:46:23 +0200

    Simple View implemented

---
+++ Lab01Kucherenko/Lab01Kucherenko/Views/ZodiacView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Controls;
+using KMA.Lab01Kucherenko.ViewModels;
+
+namespace KMA.Lab01Kucherenko.Views
+{
+    /// <summary>
+    /// Interaction logic for ZodiacView.xaml
+    /// </summary>
+    public partial class ZodiacView : UserControl
+    {
+        private ZodiacViewModel _viewModel;
+        public ZodiacView()
+        {
+            InitializeComponent();
+            DataContext = _viewModel = new ZodiacViewModel();
+        }
+    }
+}
