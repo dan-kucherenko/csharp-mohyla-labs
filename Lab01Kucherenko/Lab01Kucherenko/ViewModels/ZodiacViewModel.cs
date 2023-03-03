31742556c09de0d5046634e32ee053004e6cd2d8
dan_kucherenko <daniil.ukraine@gmail.com>
2023-03-03 15:40:16 +0200

    Age calculation added (RelayCommand class for Actions imported). Refactor the "ZodiacView"

--- Lab01Kucherenko/Lab01Kucherenko/ViewModels/ZodiacViewModel.cs
+++ Lab01Kucherenko/Lab01Kucherenko/ViewModels/ZodiacViewModel.cs
@@ -1,49 +1,114 @@
 ﻿using KMA.Lab01Kucherenko.Models;
 using KMA.Lab01Kucherenko.Tools;
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
 
 namespace KMA.Lab01Kucherenko.ViewModels
 {
-    internal class ZodiacViewModel
+    internal class ZodiacViewModel : INotifyPropertyChanged
     {
+        #region Fields
+
         private User _user = new User();
+        private RelayCommand<object> _signsRequested;
+
+        #endregion
 
         #region Properties
 
         public int Age
         {
             get => _user.Age;
-            set => _user.Age = value;
+            private set
+            {
+                _user.Age = value;
+                NotifyPropertyChanged();
+            }
         }
 
         public DateTime DateOfBirth
         {
             get => _user.DateOfBirth;
-            set => _user.DateOfBirth = value;
+            set
+            {
+                if (_user.DateOfBirth != value)
+                {
+                    _user.DateOfBirth = value;
+                    DateChanged();
+                    NotifyPropertyChanged();
+                }
+            }
         }
 
         public ZodiacSigns Zodiac
         {
             get => _user.Zodiac;
-            set => _user.Zodiac = value;
+            private set
+            {
+                _user.Zodiac = value;
+                NotifyPropertyChanged();
+            }
         }
 
         public ChineseZodiacSigns ChineseZodiac
         {
             get => _user.ChineseZodiac;
-            set => _user.ChineseZodiac = value;
+            private set
+            {
+                _user.ChineseZodiac = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public RelayCommand<object> SignsRequestedCommand =>
+            _signsRequested ??= new RelayCommand<object>(_ => GetSigns());
+
+        #endregion
+
+        private void GetSigns()
+        {
+           throw new NotImplementedException();
+        }
+
+        private void DateChanged()
+        {
+            Age = CalculateAge(DateOfBirth);
+            if (IsBirthday(DateOfBirth))
+                MessageBox.Show("Happy birthday!");
+        }
+
+        #region AgeValidationMethods
+
+        private int CalculateAge(DateTime dob)
+        {
+            var age = DateTime.Now.Year - dob.Year;
+            if (DateTime.Now.Month < dob.Month ||
+                (DateTime.Now.Month == dob.Month && DateTime.Now.Day < dob.Day))
+                age--;
+            return age;
+        }
+
+        private bool IsBirthday(DateTime dob)
+        {
+            if (!IsValidAge(CalculateAge(dob)))
+                MessageBox.Show("Invalid date of birth");
+            return DateTime.Now.DayOfYear == dob.DayOfYear;
+        }
+
+        private bool IsValidAge(int age)
+        {
+            return age is >= 0 and < 135;
         }
 
         #endregion
 
-        public void DateChanged()
-        {
-            CalculateAge(DateOfBirth);
-        }
+        public event PropertyChangedEventHandler? PropertyChanged;
 
-        private void CalculateAge(DateTime selectedDate)
+        protected virtual void NotifyPropertyChanged([CallerMemberName] string? propertyName = null)
         {
-            throw new NotImplementedException();
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
